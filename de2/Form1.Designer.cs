a4cf3b9ccb07b41b3a7104aa7a064a9830e0ca7d
hai12dz <fifahayrataolahai1762004@gmail.com>
2024-11-10 09:55:17 +0700

    de2

---
+++ de2/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace de2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBoxChatLieu = new System.Windows.Forms.ComboBox();
+            this.textBoxTenHang = new System.Windows.Forms.TextBox();
+            this.textBoxDonGiaBanDen = new System.Windows.Forms.TextBox();
+            this.textBoxDonGiaBanTu = new System.Windows.Forms.TextBox();
+            this.textBoxMaHang = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.btnInRaExcel = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.dataGridViewHienThi = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHienThi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(389, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tìm kiếm hàng hóa";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.comboBoxChatLieu);
+            this.panel1.Controls.Add(this.textBoxTenHang);
+            this.panel1.Controls.Add(this.textBoxDonGiaBanDen);
+            this.panel1.Controls.Add(this.textBoxDonGiaBanTu);
+            this.panel1.Controls.Add(this.textBoxMaHang);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(44, 142);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(780, 156);
+            this.panel1.TabIndex = 1;
+            // 
+            // comboBoxChatLieu
+            // 
+            this.comboBoxChatLieu.FormattingEnabled = true;
+            this.comboBoxChatLieu.Location = new System.Drawing.Point(599, 105);
+            this.comboBoxChatLieu.Name = "comboBoxChatLieu";
+            this.comboBoxChatLieu.Size = new System.Drawing.Size(158, 24);
+            this.comboBoxChatLieu.TabIndex = 13;
+            // 
+            // textBoxTenHang
+            // 
+            this.textBoxTenHang.Location = new System.Drawing.Point(599, 48);
+            this.textBoxTenHang.Name = "textBoxTenHang";
+            this.textBoxTenHang.Size = new System.Drawing.Size(158, 22);
+            this.textBoxTenHang.TabIndex = 12;
+            // 
+            // textBoxDonGiaBanDen
+            // 
+            this.textBoxDonGiaBanDen.Location = new System.Drawing.Point(328, 105);
+            this.textBoxDonGiaBanDen.Name = "textBoxDonGiaBanDen";
+            this.textBoxDonGiaBanDen.Size = new System.Drawing.Size(150, 22);
+            this.textBoxDonGiaBanDen.TabIndex = 11;
+            this.textBoxDonGiaBanDen.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxDonGiaBanDen_KeyPress);
+            // 
+            // textBoxDonGiaBanTu
+            // 
+            this.textBoxDonGiaBanTu.Location = new System.Drawing.Point(156, 105);
+            this.textBoxDonGiaBanTu.Name = "textBoxDonGiaBanTu";
+            this.textBoxDonGiaBanTu.Size = new System.Drawing.Size(108, 22);
+            this.textBoxDonGiaBanTu.TabIndex = 10;
+            this.textBoxDonGiaBanTu.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxDonGiaBanTu_KeyPress);
+            // 
+            // textBoxMaHang
+            // 
+            this.textBoxMaHang.Location = new System.Drawing.Point(133, 38);
+            this.textBoxMaHang.Name = "textBoxMaHang";
+            this.textBoxMaHang.Size = new System.Drawing.Size(270, 22);
+            this.textBoxMaHang.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(480, 105);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 16);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "(VNĐ)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(258, 105);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 16);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "(VNĐ) đến:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(130, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "từ:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mã hàng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Đơn giá bán";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(529, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tên hàng";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(529, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Chất liệu";
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(135, 345);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(98, 23);
+            this.btnTimKiem.TabIndex = 2;
+            this.btnTimKiem.Text = "Tìm Kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // btnInRaExcel
+            // 
+            this.btnInRaExcel.Location = new System.Drawing.Point(358, 345);
+            this.btnInRaExcel.Name = "btnInRaExcel";
+            this.btnInRaExcel.Size = new System.Drawing.Size(112, 23);
+            this.btnInRaExcel.TabIndex = 3;
+            this.btnInRaExcel.Text = "In ra Excel";
+            this.btnInRaExcel.UseVisualStyleBackColor = true;
+            this.btnInRaExcel.Click += new System.EventHandler(this.btnInRaExcel_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(557, 345);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 4;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // dataGridViewHienThi
+            // 
+            this.dataGridViewHienThi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewHienThi.Location = new System.Drawing.Point(44, 425);
+            this.dataGridViewHienThi.Name = "dataGridViewHienThi";
+            this.dataGridViewHienThi.RowHeadersWidth = 51;
+            this.dataGridViewHienThi.RowTemplate.Height = 24;
+            this.dataGridViewHienThi.Size = new System.Drawing.Size(758, 330);
+            this.dataGridViewHienThi.TabIndex = 5;
+            this.dataGridViewHienThi.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewHienThi_CellDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 805);
+            this.Controls.Add(this.dataGridViewHienThi);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnInRaExcel);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHienThi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.Button btnInRaExcel;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.DataGridView dataGridViewHienThi;
+        private System.Windows.Forms.ComboBox comboBoxChatLieu;
+        private System.Windows.Forms.TextBox textBoxTenHang;
+        private System.Windows.Forms.TextBox textBoxDonGiaBanDen;
+        private System.Windows.Forms.TextBox textBoxDonGiaBanTu;
+        private System.Windows.Forms.TextBox textBoxMaHang;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ de2/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace de2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string connectionString = @"Data Source=hai\SQLEXPRESS;Initial Catalog=DuLieu;Integrated Security=True;Encrypt=False";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT TenChatLieu FROM tblChatLieu", connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    comboBoxChatLieu.Items.Add(reader["TenChatLieu"].ToString());
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source=hai\SQLEXPRESS;Initial Catalog=DuLieu;Integrated Security=True;Encrypt=False";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT MaChatLieu, TenChatLieu FROM tblChatLieu", connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    comboBoxChatLieu.Items.Add(new
+                    {
+                        MaChatLieu = reader["MaChatLieu"].ToString(),
+                        TenChatLieu = reader["TenChatLieu"].ToString()
+                    });
+                }
+            }
+        }
+
+        private void textBoxDonGiaBanTu_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBoxDonGiaBanDen_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            // Bắt đầu xây dựng câu lệnh SQL với điều kiện mặc định
+            string query = @"
+    SELECT 
+        h.MaHang, 
+        h.TenHang, 
+        c.TenChatLieu, 
+        h.DonGiaNhap, 
+        h.DonGiaBan, 
+        h.SoLuong
+    FROM 
+        tblHang h
+    JOIN 
+        tblChatLieu c ON h.MaChatLieu = c.MaChatLieu
+    WHERE 1=1";
+
+            // Kiểm tra và thêm điều kiện cho Mã Hàng (LIKE)
+            if (!string.IsNullOrEmpty(textBoxMaHang.Text))
+            {
+                query += " AND h.MaHang LIKE @MaHang";
+            }
+
+            // Kiểm tra và thêm điều kiện cho Tên Hàng (LIKE)
+            if (!string.IsNullOrEmpty(textBoxTenHang.Text))
+            {
+                query += " AND h.TenHang LIKE @TenHang";
+            }
+
+            // Kiểm tra và thêm điều kiện cho Chất Liệu (ComboBox)
+            if (comboBoxChatLieu.SelectedItem != null)
+            {
+                query += " AND c.TenChatLieu = @ChatLieu";
+            }
+
+            // Kiểm tra và thêm điều kiện cho Đơn giá từ (>=)
+            if (!string.IsNullOrEmpty(textBoxDonGiaBanTu.Text))
+            {
+                query += " AND h.DonGiaBan >= @DonGiaBanTu";
+            }
+
+            // Kiểm tra và thêm điều kiện cho Đơn giá đến (<=)
+            if (!string.IsNullOrEmpty(textBoxDonGiaBanDen.Text))
+            {
+                query += " AND h.DonGiaBan <= @DonGiaBanDen";
+            }
+
+            // Mở kết nối đến cơ sở dữ liệu và thực hiện tìm kiếm
+            using (SqlConnection connection = new SqlConnection(@"Data Source=hai\SQLEXPRESS;Initial Catalog=DuLieu;Integrated Security=True;Encrypt=False"))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                // Thêm tham số vào câu lệnh SQL
+                if (!string.IsNullOrEmpty(textBoxMaHang.Text))
+                    command.Parameters.AddWithValue("@MaHang", "%" + textBoxMaHang.Text + "%");
+
+                if (!string.IsNullOrEmpty(textBoxTenHang.Text))
+                    command.Parameters.AddWithValue("@TenHang", "%" + textBoxTenHang.Text + "%");
+
+                if (comboBoxChatLieu.SelectedItem != null)
+                    command.Parameters.AddWithValue("@ChatLieu", comboBoxChatLieu.SelectedItem.ToString());
+
+                if (!string.IsNullOrEmpty(textBoxDonGiaBanTu.Text))
+                    command.Parameters.AddWithValue("@DonGiaBanTu", Convert.ToDecimal(textBoxDonGiaBanTu.Text));
+
+                if (!string.IsNullOrEmpty(textBoxDonGiaBanDen.Text))
+                    command.Parameters.AddWithValue("@DonGiaBanDen", Convert.ToDecimal(textBoxDonGiaBanDen.Text));
+
+                // Mở kết nối và thực thi câu lệnh
+                SqlDataAdapter adt = new SqlDataAdapter(command);
+                DataTable dt = new DataTable();
+                adt.Fill(dt);
+
+                // Hiển thị kết quả trong DataGridView
+                dataGridViewHienThi.DataSource = dt;
+            }
+        }
+
+        private void btnInRaExcel_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra nếu có dữ liệu trong DataGridView
+            if (dataGridViewHienThi.Rows.Count > 0)
+            {
+                // Tạo đối tượng Excel
+                var excelApp = new Microsoft.Office.Interop.Excel.Application();
+                excelApp.Visible = true;
+
+                // Tạo workbook và worksheet
+                var workBook = excelApp.Workbooks.Add();
+                var workSheet = (Microsoft.Office.Interop.Excel.Worksheet)workBook.Sheets[1];
+
+                // Lặp qua cột DataGridView và thêm tiêu đề vào Excel
+                for (int i = 0; i < dataGridViewHienThi.Columns.Count; i++)
+                {
+                    workSheet.Cells[1, i + 1] = dataGridViewHienThi.Columns[i].HeaderText;
+                }
+
+                // Lặp qua các dòng và thêm dữ liệu vào Excel
+                for (int i = 0; i < dataGridViewHienThi.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridViewHienThi.Columns.Count; j++)
+                    {
+                        var cellValue = dataGridViewHienThi.Rows[i].Cells[j].Value;
+                        workSheet.Cells[i + 2, j + 1] = cellValue != null ? cellValue.ToString() : string.Empty;
+
+                    }
+                }
+
+                // Lưu file Excel (tuỳ chỉnh tên file hoặc yêu cầu lưu)
+                string filePath = @"E:\zalo\trucquan\ktragiuaky\xuatFile\DanhSachMatHang.xlsx"; // Đổi đường dẫn nếu cần
+                workBook.SaveAs(filePath);
+                workBook.Close();
+                excelApp.Quit();
+
+                MessageBox.Show("Đã xuất dữ liệu ra Excel thành công.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Không có dữ liệu để xuất.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Bạn có muốn thoát không?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void dataGridViewHienThi_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Kiểm tra nếu người dùng click vào một dòng hợp lệ (không phải header)
+            if (e.RowIndex >= 0)
+            {
+                // Lấy giá trị Tên Chất Liệu từ cột TênChấtLiệu của dòng đã chọn
+                string tenChatLieu = dataGridViewHienThi.Rows[e.RowIndex].Cells["TenChatLieu"].Value.ToString();
+
+                // Tạo câu lệnh SQL để đếm số mặt hàng có chất liệu giống nhau
+                string query = "SELECT COUNT(*) FROM tblHang h JOIN tblChatLieu c ON h.MaChatLieu = c.MaChatLieu WHERE c.TenChatLieu = @TenChatLieu";
+
+                // Mở kết nối và thực hiện truy vấn
+                using (SqlConnection connection = new SqlConnection(@"Data Source=hai\SQLEXPRESS;Initial Catalog=DuLieu;Integrated Security=True;Encrypt=False"))
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@TenChatLieu", tenChatLieu);
+
+                    connection.Open();
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+
+                    // Hiển thị thông báo với số lượng mặt hàng có chất liệu giống nhau
+                    MessageBox.Show($"Có {count} mặt hàng có chất liệu '{tenChatLieu}'.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+     
+    }
+}
